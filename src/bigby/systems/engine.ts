c7923a6a952b1e1749a79914194d2772ccc664b8
Hendrik Mans <hendrik@mans.de>
2022-11-25 18:33:43 +0100

    bind frame buffer every frame (?)

--- src/bigby/systems/engine.ts
+++ src/bigby/systems/engine.ts
@@ -34,6 +34,9 @@ export default (world: World<Entity>) => {
     gl.clearColor(0, 0, 0, 0)
     gl.clear(gl.COLOR_BUFFER_BIT)
 
+    gl.bindFramebuffer(gl.FRAMEBUFFER, null)
+    gl.viewport(0, 0, canvas.width, canvas.height)
+
     /* Draw */
     for (const { mesh } of meshes) {
       mesh.render(gl)
