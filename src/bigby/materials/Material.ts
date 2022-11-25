96ae6eb590eece7f8e30b841294765561e6d05d4
Hendrik Mans <hendrik@mans.de>
2022-11-25 17:59:14 +0100

    Cleanup

--- src/bigby/materials/Material.ts
+++ src/bigby/materials/Material.ts
@@ -1,8 +1,21 @@
 import { createProgram, createShader } from "../helpers"
 
 export class Material {
-  readonly vertexShader!: string
-  readonly fragmentFragment!: string
+  readonly vertexShader = /*glsl*/ `#version 300 es
+    in vec4 a_position;
+    void main() {
+      gl_Position = a_position;
+    }
+  `
+
+  readonly fragmentShader = /*glsl*/ `#version 300 es
+    precision highp float;
+    out vec4 outColor;
+    
+    void main() {
+      outColor = vec4(1, 0, 0.5, 1);
+    }
+  `
 
   program?: WebGLProgram
 
@@ -11,31 +24,11 @@ export class Material {
   }
 
   compile(gl: WebGL2RenderingContext) {
-    const vertexShader = createShader(
-      gl,
-      gl.VERTEX_SHADER,
-      /*glsl*/ `#version 300 es
-        in vec4 a_position;
-        void main() {
-          gl_Position = a_position;
-        }
-      `
-    )
-
-    const fragmentShader = createShader(
-      gl,
-      gl.FRAGMENT_SHADER,
-      /*glsl*/ `#version 300 es
-        precision highp float;
-        out vec4 outColor;
-        
-        void main() {
-          outColor = vec4(1, 0, 0.5, 1);
-        }
-      `
-    )
+    const vertexShader = createShader(gl, gl.VERTEX_SHADER, this.vertexShader)
+    const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, this.fragmentShader)
 
     this.program = createProgram(gl, vertexShader, fragmentShader)
+
     gl.deleteShader(vertexShader)
     gl.deleteShader(fragmentShader)
   }
