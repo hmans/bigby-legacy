ef43da034533cf1a88e863bb66e97ea9f87cbf51
Hendrik Mans <hendrik@mans.de>
2022-11-25 18:11:33 +0100

    Move rendering to Mesh class... for now

--- src/bigby/core/Mesh.ts
+++ src/bigby/core/Mesh.ts
@@ -10,6 +10,9 @@ export class Mesh {
   }
 
   compile(gl: WebGL2RenderingContext) {
+    /* Ensure that material is compiled */
+    if (!this.material.isCompiled) this.material.compile(gl)
+
     /* Fill the position buffer */
     const positionBuffer = gl.createBuffer()
     gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
@@ -23,6 +26,39 @@ export class Mesh {
     const vao = gl.createVertexArray()!
     gl.bindVertexArray(vao)
 
+    const positionAttributeLocation = gl.getAttribLocation(
+      this.material.program!,
+      "a_position"
+    )
+
+    var size = 2 // 2 components per iteration
+    var type = gl.FLOAT // the data is 32bit floats
+    var normalize = false // don't normalize the data
+    var stride = 0 // 0 = move forward size * sizeof(type) each iteration to get the next position
+    var offset = 0 // start at the beginning of the buffer
+    gl.vertexAttribPointer(
+      positionAttributeLocation,
+      size,
+      type,
+      normalize,
+      stride,
+      offset
+    )
+
+    gl.enableVertexAttribArray(positionAttributeLocation)
+
     this.vao = vao
+  }
+
+  render(gl: WebGL2RenderingContext) {
+    if (!this.isCompiled) this.compile(gl)
+
+    gl.useProgram(this.material.program!)
+    gl.bindVertexArray(this.vao!)
+
+    var primitiveType = gl.TRIANGLES
+    var offset = 0
+    var count = 3
+    gl.drawArrays(primitiveType, offset, count)
   }
 }
--- src/bigby/systems/engine.ts
+++ src/bigby/systems/engine.ts
@@ -36,45 +36,7 @@ export default (world: World<Entity>) => {
 
     /* Draw */
     for (const { mesh } of meshes) {
-      if (!mesh.material.isCompiled) mesh.material.compile(gl)
-      if (!mesh.isCompiled) mesh.compile(gl)
-
-      /* Prepare material */
-      gl.useProgram(mesh.material.program!)
-
-      /* Prepare geometry */
-
-      /*
-      NOTE: I don't know if we're supposed to do all this every frame. Still trying
-      to understand VAOs!
-      */
-      gl.bindVertexArray(mesh.vao!)
-
-      const positionAttributeLocation = gl.getAttribLocation(
-        mesh.material.program!,
-        "a_position"
-      )
-
-      var size = 2 // 2 components per iteration
-      var type = gl.FLOAT // the data is 32bit floats
-      var normalize = false // don't normalize the data
-      var stride = 0 // 0 = move forward size * sizeof(type) each iteration to get the next position
-      var offset = 0 // start at the beginning of the buffer
-      gl.vertexAttribPointer(
-        positionAttributeLocation,
-        size,
-        type,
-        normalize,
-        stride,
-        offset
-      )
-
-      gl.enableVertexAttribArray(positionAttributeLocation)
-
-      var primitiveType = gl.TRIANGLES
-      var offset = 0
-      var count = 3
-      gl.drawArrays(primitiveType, offset, count)
+      mesh.render(gl)
     }
   }
 }
