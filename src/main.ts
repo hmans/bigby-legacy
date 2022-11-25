abc21d67dcf8b11e8676391ca3fd5a0d9d118c77
Hendrik Mans <hendrik@mans.de>
2022-11-25 09:50:38 +0100

    Let's go... again

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,23 @@
-import { Game } from "./bigby/game"
 import "./style.css"
+import { World } from "@miniplex/core"
 
-const game = new Game()
+type Entity = {
+  engine?: true
+}
+
+const world = new World<Entity>()
+
+world.add({ engine: true })
+
+console.log("Let's go! 🐝")
+
+/* Initialize canvas */
+const canvas = document.body.appendChild(document.createElement("canvas"))
+
+/* Initialize WebGL */
+const gl = canvas.getContext("webgl2", {
+  antialias: true,
+  powerPreference: "high-performance"
+})!
+
+if (!gl) throw new Error("WebGL2 not supported")
