9f5a16bbcfca8987970de6c38e0b534400b937ae
Hendrik Mans <hendrik@mans.de>
2022-11-25 13:20:38 +0100

    Extract functions

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import "./style.css"
 import { World } from "@miniplex/core"
+import { createProgram, createShader } from "./bigby/helpers"
 
 type Entity = {
   engine?: true
@@ -23,41 +24,6 @@ const gl = canvas.getContext("webgl2", {
 })!
 
 if (!gl) throw new Error("WebGL2 not supported")
-
-function createShader(
-  gl: WebGL2RenderingContext,
-  type: number,
-  source: string
-) {
-  const shader = gl.createShader(type)
-  if (!shader) throw new Error("Failed to create shader")
-
-  gl.shaderSource(shader, source)
-  gl.compileShader(shader)
-
-  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS))
-    throw new Error("Failed to compile shader: " + gl.getShaderInfoLog(shader))
-
-  return shader
-}
-
-function createProgram(
-  gl: WebGL2RenderingContext,
-  vertexShader: WebGLShader,
-  fragmentShader: WebGLShader
-) {
-  const program = gl.createProgram()
-  if (!program) throw new Error("Failed to create program")
-
-  gl.attachShader(program, vertexShader)
-  gl.attachShader(program, fragmentShader)
-  gl.linkProgram(program)
-
-  if (!gl.getProgramParameter(program, gl.LINK_STATUS))
-    throw new Error("Failed to link program")
-
-  return program
-}
 
 const vertexShader = createShader(
   gl,
