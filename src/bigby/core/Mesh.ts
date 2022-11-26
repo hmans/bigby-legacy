f3b98a90e77b84b42f74d8d45da40891c3c479b3
Hendrik Mans <hendrik@mans.de>
2022-11-26 18:46:52 +0100

    Move mesh rendering code into main render loop

--- src/bigby/core/Mesh.ts
+++ src/bigby/core/Mesh.ts
@@ -1,7 +1,5 @@
-import { mat4 } from "gl-matrix"
 import { Geometry } from "../geometry/Geometry"
 import { Material } from "../materials/Material"
-import { Transform } from "./Transform"
 
 export class Mesh {
   vao?: WebGLVertexArrayObject
@@ -41,75 +39,6 @@ export class Mesh {
         gl.enableVertexAttribArray(location)
         gl.vertexAttribPointer(location, attribute.size, gl.FLOAT, false, 0, 0)
       }
-    }
-  }
-
-  render(gl: WebGL2RenderingContext, transform: Transform) {
-    /* Ensure that the mesh is compiled */
-    if (!this.isCompiled) this.compile(gl)
-
-    /* Use this mesh's material's program */
-    gl.useProgram(this.material.program!)
-
-    /* Update the material's uniforms */
-    /* TODO: Change this so this only happens once per frame per material */
-    this.material.updateUniforms(gl)
-
-    /* Update modelMatrix uniform */
-    const location = gl.getUniformLocation(this.material.program!, "modelMatrix")
-    if (location !== null) gl.uniformMatrix4fv(location, false, transform.matrix)
-
-    /* Update viewMatrix uniform */
-    const viewMatrix = mat4.create()
-    mat4.lookAt(viewMatrix, [0, 0, 5], [0, 0, 0], [0, 1, 0])
-    const viewLocation = gl.getUniformLocation(this.material.program!, "viewMatrix")
-    if (viewLocation !== null) gl.uniformMatrix4fv(viewLocation, false, viewMatrix)
-
-    /* Update projectionMatrix uniform */
-    const perspectiveMatrix = mat4.create()
-    const projectionLocation = gl.getUniformLocation(
-      this.material.program!,
-      "projectionMatrix"
-    )
-    if (projectionLocation !== null)
-      gl.uniformMatrix4fv(
-        projectionLocation,
-        false,
-        mat4.perspectiveNO(
-          perspectiveMatrix,
-          75 * (Math.PI / 180),
-          gl.canvas.width / gl.canvas.height,
-          0.1,
-          1000
-        )
-      )
-
-    /* Draw the geometry */
-    gl.bindVertexArray(this.vao!)
-
-    /* Cull back faces... for now */
-    gl.enable(gl.CULL_FACE)
-    gl.cullFace(gl.BACK)
-
-    /* Enable depth testing */
-    gl.enable(gl.DEPTH_TEST)
-
-    if (this.geometry.attributes.index) {
-      gl.drawElements(
-        gl.TRIANGLES,
-        this.geometry.attributes.index.data.length /
-          this.geometry.attributes.index.size,
-        gl.UNSIGNED_INT,
-        0
-      )
-    } else {
-      gl.drawArraysInstanced(
-        gl.TRIANGLES,
-        0,
-        this.geometry.attributes.position.data.length /
-          this.geometry.attributes.position.size,
-        1
-      )
     }
   }
 }
--- src/bigby/systems/engine.ts
+++ src/bigby/systems/engine.ts
@@ -1,5 +1,5 @@
 import { With, World } from "@miniplex/core"
-import { vec3 } from "gl-matrix"
+import { mat4, vec3 } from "gl-matrix"
 import { Mesh } from "../core/Mesh"
 import { Transform } from "../core/Transform"
 
@@ -41,8 +41,75 @@ export default (world: World<Entity>) => {
 
     /* Draw */
     for (const { mesh, transform } of meshes) {
-      /* Render mesh */
-      mesh.render(gl, transform)
+      /* Ensure that the mesh is compiled */
+      if (!mesh.isCompiled) mesh.compile(gl)
+
+      /* Use this mesh's material's program */
+      gl.useProgram(mesh.material.program!)
+
+      /* Update the material's uniforms */
+      /* TODO: Change this so this only happens once per frame per material */
+      mesh.material.updateUniforms(gl)
+
+      /* Update modelMatrix uniform */
+      const location = gl.getUniformLocation(mesh.material.program!, "modelMatrix")
+      if (location !== null) gl.uniformMatrix4fv(location, false, transform.matrix)
+
+      /* Update viewMatrix uniform */
+      const viewMatrix = mat4.create()
+      mat4.lookAt(viewMatrix, [0, 0, 5], [0, 0, 0], [0, 1, 0])
+      const viewLocation = gl.getUniformLocation(
+        mesh.material.program!,
+        "viewMatrix"
+      )
+      if (viewLocation !== null) gl.uniformMatrix4fv(viewLocation, false, viewMatrix)
+
+      /* Update projectionMatrix uniform */
+      const perspectiveMatrix = mat4.create()
+      const projectionLocation = gl.getUniformLocation(
+        mesh.material.program!,
+        "projectionMatrix"
+      )
+      if (projectionLocation !== null)
+        gl.uniformMatrix4fv(
+          projectionLocation,
+          false,
+          mat4.perspectiveNO(
+            perspectiveMatrix,
+            75 * (Math.PI / 180),
+            gl.canvas.width / gl.canvas.height,
+            0.1,
+            1000
+          )
+        )
+
+      /* Draw the geometry */
+      gl.bindVertexArray(mesh.vao!)
+
+      /* Cull back faces... for now */
+      gl.enable(gl.CULL_FACE)
+      gl.cullFace(gl.BACK)
+
+      /* Enable depth testing */
+      gl.enable(gl.DEPTH_TEST)
+
+      if (mesh.geometry.attributes.index) {
+        gl.drawElements(
+          gl.TRIANGLES,
+          mesh.geometry.attributes.index.data.length /
+            mesh.geometry.attributes.index.size,
+          gl.UNSIGNED_INT,
+          0
+        )
+      } else {
+        gl.drawArraysInstanced(
+          gl.TRIANGLES,
+          0,
+          mesh.geometry.attributes.position.data.length /
+            mesh.geometry.attributes.position.size,
+          1
+        )
+      }
     }
   }
 }
