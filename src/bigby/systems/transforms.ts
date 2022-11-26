66fd8c6f02a29dd14f68d750284298ad5d373360
Hendrik Mans <hendrik@mans.de>
2022-11-26 12:28:39 +0100

    Rotate!

--- src/bigby/systems/transforms.ts
+++ src/bigby/systems/transforms.ts
@@ -1,5 +1,5 @@
 import { World } from "@miniplex/core"
-import { mat4 } from "gl-matrix"
+import { mat4, quat } from "gl-matrix"
 import { Entity } from "./engine"
 
 export default (world: World<Entity>) => {
@@ -8,6 +8,8 @@ export default (world: World<Entity>) => {
   return () => {
     for (const { transform } of entities) {
       if (!transform.autoUpdate) continue
+
+      quat.rotateZ(transform.quaternion, transform.quaternion, 0.01)
 
       mat4.fromRotationTranslationScale(
         transform.matrix,
