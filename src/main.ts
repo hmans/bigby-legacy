fff252d5f80513ba821f5e8333e5d0d937025415
Hendrik Mans <hendrik@mans.de>
2022-11-27 09:42:45 +0100

    Apparently we don't have to await import

--- src/main.ts
+++ src/main.ts
@@ -10,27 +10,25 @@ import { Material } from "./bigby/materials/Material"
 import { RigidBody } from "./bigby/systems/physics"
 import "./style.css"
 
-import("@dimforge/rapier3d").then(() => {
-  new App((world) => {
-    world.add({
-      // autorotate: [1, 1.3, 0],
-      transform: new Transform([0, 0, 20]),
-      camera: new Camera(70, 0.1, 1000),
-    })
-
-    const geometry = new BoxGeometry()
-    const material = new Material({ color: new Color(0x00ff00) })
-
-    for (let i = 0; i < 100; i++) {
-      world.add({
-        // autorotate: [plusMinus(1), plusMinus(1), 0],
-        transform: new Transform(
-          [plusMinus(20), plusMinus(10), 0],
-          quat.random(quat.create())
-        ),
-        mesh: new Mesh(geometry, material),
-        rigidbody: new RigidBody(),
-      })
-    }
+new App((world) => {
+  world.add({
+    // autorotate: [1, 1.3, 0],
+    transform: new Transform([0, 0, 20]),
+    camera: new Camera(70, 0.1, 1000),
   })
+
+  const geometry = new BoxGeometry()
+  const material = new Material({ color: new Color(0x00ff00) })
+
+  for (let i = 0; i < 100; i++) {
+    world.add({
+      // autorotate: [plusMinus(1), plusMinus(1), 0],
+      transform: new Transform(
+        [plusMinus(20), plusMinus(10), 0],
+        quat.random(quat.create())
+      ),
+      mesh: new Mesh(geometry, material),
+      rigidbody: new RigidBody(),
+    })
+  }
 })
