9bb8256725afc6e668bad16a03ea0313acf6600b
Hendrik Mans <hendrik@mans.de>
2022-11-27 13:42:29 +0100

    Fix production

--- src/main.ts
+++ src/main.ts
@@ -6,36 +6,37 @@ import { Camera } from "./bigby/core/Camera"
 import { Mesh } from "./bigby/core/Mesh"
 import { Transform } from "./bigby/core/Transform"
 import { BoxGeometry } from "./bigby/geometry/BoxGeometry"
+
 import { Material } from "./bigby/materials/Material"
 import AutorotatePlugin from "./bigby/plugins/autorotate"
 import PhysicsPlugin, { RigidBody } from "./bigby/plugins/physics"
 import RenderingPlugin from "./bigby/plugins/rendering"
 import "./style.css"
 
-await import("@dimforge/rapier3d")
+import("@dimforge/rapier3d").then(() => {
+  const app = new App()
+    .addPlugin(PhysicsPlugin)
+    .addPlugin(AutorotatePlugin)
+    .addPlugin(RenderingPlugin)
 
-const app = new App()
-  .addPlugin(PhysicsPlugin)
-  .addPlugin(AutorotatePlugin)
-  .addPlugin(RenderingPlugin)
-
-app.world.add({
-  transform: new Transform([0, 0, 20]),
-  camera: new Camera(70, 0.1, 1000),
-})
-
-const geometry = new BoxGeometry()
-const material = new Material({
-  color: new Color("hotpink"),
-})
-
-for (let i = 0; i < 100; i++) {
   app.world.add({
-    transform: new Transform(
-      [plusMinus(20), plusMinus(10), 0],
-      quat.random(quat.create())
-    ),
-    mesh: new Mesh(geometry, material),
-    rigidbody: new RigidBody(),
+    transform: new Transform([0, 0, 20]),
+    camera: new Camera(70, 0.1, 1000),
   })
-}
+
+  const geometry = new BoxGeometry()
+  const material = new Material({
+    color: new Color("hotpink"),
+  })
+
+  for (let i = 0; i < 100; i++) {
+    app.world.add({
+      transform: new Transform(
+        [plusMinus(20), plusMinus(10), 0],
+        quat.random(quat.create())
+      ),
+      mesh: new Mesh(geometry, material),
+      rigidbody: new RigidBody(),
+    })
+  }
+})
