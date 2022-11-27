5548fb9a2214b79c1e6aa2a110185525cdf0401f
Hendrik Mans <hendrik@mans.de>
2022-11-27 07:13:54 +0100

    App

---
+++ src/bigby/App.ts
@@ -0,0 +1,38 @@
+import { World } from "@miniplex/core"
+import { Entity } from "./Entity"
+import autorotate from "./systems/autorotate"
+import rendering from "./systems/rendering"
+import transforms from "./systems/transforms"
+
+export class App {
+  world: World<Entity>
+
+  constructor(setup?: (world: World<Entity>) => void) {
+    this.world = new World<Entity>()
+
+    const systems = [
+      autorotate(this.world),
+      transforms(this.world),
+      rendering(this.world),
+    ]
+
+    setup?.(this.world)
+
+    /* Tick */
+    let lastTime = performance.now()
+
+    function animate() {
+      requestAnimationFrame(animate)
+
+      /* Calculate delta time */
+      const time = performance.now()
+      const dt = (time - lastTime) / 1000
+      lastTime = time
+
+      /* Update systems */
+      systems.forEach((system) => system(dt))
+    }
+
+    animate()
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,73 +1,49 @@
-import { World } from "@miniplex/core"
-import { vec3 } from "gl-matrix"
 import { Color } from "three"
+import { App } from "./bigby/App"
+import { Camera } from "./bigby/core/Camera"
 import { Mesh } from "./bigby/core/Mesh"
 import { Transform } from "./bigby/core/Transform"
 import { BoxGeometry } from "./bigby/geometry/BoxGeometry"
 import { Material } from "./bigby/materials/Material"
-import autorotate from "./bigby/systems/autorotate"
-import rendering from "./bigby/systems/rendering"
-import { Entity } from "./bigby/Entity"
-import transforms from "./bigby/systems/transforms"
 import "./style.css"
-import { Camera } from "./bigby/core/Camera"
 
-const world = new World<Entity>()
+new App((world) => {
+  world.add({
+    // autorotate: [1, 1.3, 0],
+    transform: new Transform([0, 0, 5]),
+    camera: new Camera(70, 0.1, 1000),
+  })
 
-const systems = [autorotate(world), transforms(world), rendering(world)]
+  world.add({
+    autorotate: [0.3, 0.1, 0.2],
+    transform: new Transform([0, 0, -30], [0, 0, 0, 1], [20, 20, 20]),
+    mesh: new Mesh(
+      new BoxGeometry(),
+      new Material({
+        color: new Color("orange"),
+      })
+    ),
+  })
 
-world.add({
-  // autorotate: [1, 1.3, 0],
-  transform: new Transform([0, 0, 5]),
-  camera: new Camera(70, 0.1, 1000),
+  world.add({
+    autorotate: [1, 1.3, 0],
+    transform: new Transform([-2, 0, 0]),
+    mesh: new Mesh(
+      new BoxGeometry(),
+      new Material({
+        color: new Color("hotpink"),
+      })
+    ),
+  })
+
+  world.add({
+    autorotate: [0, -1, 1.3],
+    transform: new Transform([2, 0, 0]),
+    mesh: new Mesh(
+      new BoxGeometry(),
+      new Material({
+        color: new Color("cyan"),
+      })
+    ),
+  })
 })
-
-world.add({
-  autorotate: [0.3, 0.1, 0.2],
-  transform: new Transform([0, 0, -30], [0, 0, 0, 1], [20, 20, 20]),
-  mesh: new Mesh(
-    new BoxGeometry(),
-    new Material({
-      color: new Color("orange"),
-    })
-  ),
-})
-
-world.add({
-  autorotate: [1, 1.3, 0],
-  transform: new Transform([-2, 0, 0]),
-  mesh: new Mesh(
-    new BoxGeometry(),
-    new Material({
-      color: new Color("hotpink"),
-    })
-  ),
-})
-
-world.add({
-  autorotate: [0, -1, 1.3],
-  transform: new Transform([2, 0, 0]),
-  mesh: new Mesh(
-    new BoxGeometry(),
-    new Material({
-      color: new Color("cyan"),
-    })
-  ),
-})
-
-/* Tick */
-let lastTime = performance.now()
-
-function animate() {
-  requestAnimationFrame(animate)
-
-  /* Calculate delta time */
-  const time = performance.now()
-  const dt = (time - lastTime) / 1000
-  lastTime = time
-
-  /* Update systems */
-  systems.forEach((system) => system(dt))
-}
-
-animate()
