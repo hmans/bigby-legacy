d5ae07a7d507c9699e8df3c4e6c6c81849341962
Hendrik Mans <hendrik@mans.de>
2022-12-01 20:39:54 +0100

    Render something

--- packages/bigby-plugin-three/src/index.ts
+++ packages/bigby-plugin-three/src/index.ts
@@ -1,6 +1,32 @@
 import { App } from "@bigby/core"
+import * as THREE from "three"
 
 export const ThreePlugin = (app: App) =>
   app.addStartupSystem((app) => {
-    console.log("hello!")
+    console.log("Initializing Three.js!")
+
+    const scene = new THREE.Scene()
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    )
+    camera.position.z = 10
+
+    const renderer = new THREE.WebGLRenderer()
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    document.body.appendChild(renderer.domElement)
+
+    scene.add(
+      new THREE.Mesh(
+        new THREE.BoxGeometry(1, 1, 1),
+        new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+      )
+    )
+
+    /* Render loop */
+    app.addSystem((dt) => {
+      renderer.render(scene, camera)
+    })
   })
