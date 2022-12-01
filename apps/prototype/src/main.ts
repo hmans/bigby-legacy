d45a3695d58a923158272d1b75824287c8c2e0b7
Hendrik Mans <hendrik@mans.de>
2022-12-01 20:14:33 +0100

    Update example

--- apps/prototype/src/main.ts
+++ apps/prototype/src/main.ts
@@ -17,8 +17,43 @@ import { quat } from "gl-matrix"
 import { plusMinus } from "randomish"
 import "./style.css"
 
+/* The app! */
+new App()
+  /* We'll need some system plugins to make it do something. */
+  .addPlugin(TickerPlugin)
+  .addPlugin(TransformsPlugin)
+  .addPlugin(WebGL2RenderingPlugin)
+  .addPlugin(PhysicsPlugin)
+
+  /* Let's add some game-specific plugins. */
+  .addPlugin(InputPlugin)
+  .addPlugin(PlayerPlugin)
+
+  /* Create stuff on startup. */
+  .addStartupSystem((app) => {
+    app.world.add([new Transform([0, 0, 20]), new Camera(70, 0.1, 1000)])
+
+    const geometry = new BoxGeometry()
+    const material = new Material({
+      color: { r: 0.5, g: 0.5, b: 0.5 },
+    })
+
+    for (let i = 0; i < 200; i++) {
+      app.world.add([
+        new Transform([plusMinus(30), plusMinus(30), 0], quat.random(quat.create())),
+        new Mesh(geometry, material),
+        new RigidBody(),
+      ])
+    }
+  })
+
+  .run()
+
+/* Components are class instances. */
 class Player {}
 
+/* A custom plugin that adds and manages a player entity. Plugins
+are just functions that get passed a reference to the app. */
 function PlayerPlugin(app: App) {
   const entities = app.world.query([Player, Input, RigidBody])
 
@@ -40,30 +75,3 @@ function PlayerPlugin(app: App) {
 
   return app
 }
-
-new App()
-  .addPlugin(TickerPlugin)
-  .addPlugin(TransformsPlugin)
-  .addPlugin(WebGL2RenderingPlugin)
-  .addPlugin(PhysicsPlugin)
-  .addPlugin(InputPlugin)
-  .addPlugin(PlayerPlugin)
-
-  .addStartupSystem((app) => {
-    app.world.add([new Transform([0, 0, 20]), new Camera(70, 0.1, 1000)])
-
-    const geometry = new BoxGeometry()
-    const material = new Material({
-      color: { r: 0.5, g: 0.5, b: 0.5 },
-    })
-
-    for (let i = 0; i < 200; i++) {
-      app.world.add([
-        new Transform([plusMinus(30), plusMinus(30), 0], quat.random(quat.create())),
-        new Mesh(geometry, material),
-        new RigidBody(),
-      ])
-    }
-  })
-
-  .run()
