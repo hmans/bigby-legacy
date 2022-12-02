47f81ce582113be89c1aab4a5e307f79aa22f07f
Hendrik Mans <hendrik@mans.de>
2022-12-02 11:52:03 +0100

    Let's go

--- apps/spacerage/src/main.tsx
+++ apps/spacerage/src/main.tsx
@@ -1,4 +1,34 @@
-import { App } from "bigby"
+import { InputPlugin } from "@bigby/plugin-input"
+import { PhysicsPlugin } from "@bigby/plugin-physics3d"
+import { ThreePlugin } from "@bigby/plugin-three"
+import { App, TickerPlugin, Transform, TransformsPlugin } from "bigby"
+import * as THREE from "three"
 import "./index.css"
 
-new App().start()
+new App()
+  .addPlugin(TickerPlugin)
+  .addPlugin(TransformsPlugin)
+  .addPlugin(ThreePlugin)
+  .addPlugin(InputPlugin)
+  .addPlugin(PhysicsPlugin)
+
+  .addStartupSystem((app) => {
+    app.world.add([
+      new Transform([0, 0, 10]),
+      new THREE.PerspectiveCamera(
+        75,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        1000
+      )
+    ])
+
+    app.world.add([
+      new Transform([0, 0, 0]),
+      new THREE.Mesh(
+        new THREE.BoxGeometry(1, 1, 1),
+        new THREE.MeshBasicMaterial({ color: "hotpink" })
+      )
+    ])
+  })
+  .start()
