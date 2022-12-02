33562d3c2eec4660079e3f90726dcfb009491700
Hendrik Mans <hendrik@mans.de>
2022-12-02 12:10:01 +0100

    Make gravity configurable

--- apps/spacerage/src/main.tsx
+++ apps/spacerage/src/main.tsx
@@ -1,16 +1,18 @@
-import { InputPlugin } from "@bigby/plugin-input"
-import { PhysicsPlugin } from "@bigby/plugin-physics3d"
+import { Input, InputPlugin } from "@bigby/plugin-input"
+import { PhysicsPlugin, RigidBody } from "@bigby/plugin-physics3d"
 import { ThreePlugin } from "@bigby/plugin-three"
 import { App, TickerPlugin, Transform, TransformsPlugin } from "bigby"
 import * as THREE from "three"
 import "./index.css"
+
+class Player {}
 
 new App()
   .addPlugin(TickerPlugin)
   .addPlugin(TransformsPlugin)
   .addPlugin(ThreePlugin)
   .addPlugin(InputPlugin)
-  .addPlugin(PhysicsPlugin)
+  .addPlugin(PhysicsPlugin({ gravity: [0, 0, 0] }))
 
   .addStartupSystem((app) => {
     /* Camera */
@@ -37,6 +39,9 @@ new App()
 
     /* Player */
     app.world.add([
+      new Player(),
+      new Input(),
+      new RigidBody(),
       new Transform([0, -8, 0]),
       new THREE.Mesh(
         new THREE.BoxGeometry(5, 1, 1),
@@ -48,6 +53,7 @@ new App()
     for (let x = -3; x <= 3; x++) {
       for (let y = -2; y <= 2; y++) {
         app.world.add([
+          new RigidBody(),
           new Transform([x * 3, y * 2 + 2, 0]),
 
           new THREE.Mesh(
@@ -57,5 +63,19 @@ new App()
         ])
       }
     }
+
+    const playerQuery = app.world.query([Player])
+
+    app.addSystem((app) => {
+      const player = playerQuery.first
+
+      if (player) {
+        const input = player.get(Input)!
+        const rigidbody = player.get(RigidBody)!
+
+        rigidbody.rigidBody?.resetForces(false)
+        rigidbody.rigidBody?.applyImpulse({ ...input, z: 0 }, true)
+      }
+    })
   })
   .start()
--- packages/bigby-plugin-physics3d/src/index.ts
+++ packages/bigby-plugin-physics3d/src/index.ts
@@ -8,73 +8,76 @@ export class RigidBody {
   collider?: RAPIER.Collider
 }
 
-function PhysicsSystem(app: App) {
-  const physics = new RAPIER.World({ x: 0, y: 0, z: 0 })
+export const PhysicsPlugin =
+  ({ gravity = [0, -9.81, 0] }: { gravity?: vec3 } = {}) =>
+  (app: App) =>
+    app
+      .addInitializer(async function () {
+        await RAPIER.init()
+      })
+      .addStartupSystem((app) => {
+        const physics = new RAPIER.World({
+          x: gravity[0],
+          y: gravity[1],
+          z: gravity[2]
+        })
 
-  const entities = app.world.query([Transform, RigidBody])
+        const entities = app.world.query([Transform, RigidBody])
 
-  entities.onEntityAdded.add((entity) => {
-    let rigidBodyDesc = RAPIER.RigidBodyDesc.dynamic()
+        entities.onEntityAdded.add((entity) => {
+          let rigidBodyDesc = RAPIER.RigidBodyDesc.dynamic()
 
-    const transform = entity.get(Transform)!
-    const rigidbody = entity.get(RigidBody)!
+          const transform = entity.get(Transform)!
+          const rigidbody = entity.get(RigidBody)!
 
-    rigidBodyDesc.setTranslation(
-      transform.position[0],
-      transform.position[1],
-      transform.position[2]
-    )
+          rigidBodyDesc.setTranslation(
+            transform.position[0],
+            transform.position[1],
+            transform.position[2]
+          )
 
-    rigidBodyDesc.setRotation({
-      x: transform.quaternion[0],
-      y: transform.quaternion[1],
-      z: transform.quaternion[2],
-      w: transform.quaternion[3]
-    })
+          rigidBodyDesc.setRotation({
+            x: transform.quaternion[0],
+            y: transform.quaternion[1],
+            z: transform.quaternion[2],
+            w: transform.quaternion[3]
+          })
 
-    rigidBodyDesc.enabledTranslations(true, true, false)
+          rigidBodyDesc.enabledTranslations(true, true, false)
 
-    rigidBodyDesc.setLinearDamping(0.5)
-    rigidBodyDesc.setAngularDamping(0.5)
+          rigidBodyDesc.setLinearDamping(0.5)
+          rigidBodyDesc.setAngularDamping(0.5)
 
-    rigidbody.rigidBody = physics.createRigidBody(rigidBodyDesc)
+          rigidbody.rigidBody = physics.createRigidBody(rigidBodyDesc)
 
-    // Create a cuboid collider attached to the dynamic rigidBody.
-    let colliderDesc = RAPIER.ColliderDesc.cuboid(0.5, 0.5, 0.5).setDensity(5.0)
-    rigidbody.collider = physics.createCollider(
-      colliderDesc,
-      rigidbody.rigidBody
-    )
-  })
+          // Create a cuboid collider attached to the dynamic rigidBody.
+          let colliderDesc = RAPIER.ColliderDesc.cuboid(
+            0.5,
+            0.5,
+            0.5
+          ).setDensity(5.0)
+          rigidbody.collider = physics.createCollider(
+            colliderDesc,
+            rigidbody.rigidBody
+          )
+        })
 
-  return (dt: number) => {
-    physics.timestep = clamp(dt, 0.01, 0.2)
-    physics.step()
+        app.addSystem((dt: number) => {
+          physics.timestep = clamp(dt, 0.01, 0.2)
+          physics.step()
 
-    entities.iterate((entity, [transform, rigidbody]) => {
-      const position = rigidbody.rigidBody!.translation()
-      vec3.set(transform.position, position.x, position.y, position.z)
+          entities.iterate((entity, [transform, rigidbody]) => {
+            const position = rigidbody.rigidBody!.translation()
+            vec3.set(transform.position, position.x, position.y, position.z)
 
-      const rotation = rigidbody.rigidBody!.rotation()
-      quat.set(
-        transform.quaternion,
-        rotation.x,
-        rotation.y,
-        rotation.z,
-        rotation.w
-      )
-    })
-  }
-}
-
-export function PhysicsPlugin(app: App) {
-  app.addInitializer(async function () {
-    await RAPIER.init()
-  })
-
-  app.addStartupSystem((app) => {
-    app.addSystem(PhysicsSystem(app))
-  })
-
-  return app
-}
+            const rotation = rigidbody.rigidBody!.rotation()
+            quat.set(
+              transform.quaternion,
+              rotation.x,
+              rotation.y,
+              rotation.z,
+              rotation.w
+            )
+          })
+        })
+      })
