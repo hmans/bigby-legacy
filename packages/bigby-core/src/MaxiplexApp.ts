4dc27a36b94bc2ec0bbe96ccb5212da859ed38e4
Hendrik Mans <hendrik@mans.de>
2022-12-04 07:10:40 +0100

    Separate the two apps

---
+++ packages/bigby-core/src/MaxiplexApp.ts
@@ -0,0 +1,83 @@
+import { World } from "@bigby/ecs"
+import { Event } from "@bigby/event"
+import {
+  OnLoadCallback,
+  Plugin,
+  OnStartCallback,
+  OnStopCallback,
+  OnUpdateCallback
+} from "./types"
+
+export type BaseEntity = {}
+
+export class App extends World {
+  onLoadCallbacks = new Array<OnLoadCallback>()
+  onStartCallbacks = new Array<OnStartCallback>()
+  onEarlyUpdateCallbacks = new Event<number>()
+  onUpdateCallbacks = new Event<number>()
+  onLateUpdateCallbacks = new Event<number>()
+  onRenderCallbacks = new Event<number>()
+  onStopCallbacks = new Event<App>()
+
+  constructor() {
+    console.log("🐝 Bigby Initializing")
+    super()
+  }
+
+  use(plugin: Plugin): App {
+    return plugin(this as any)
+  }
+
+  onLoad(callback: OnLoadCallback) {
+    this.onLoadCallbacks.push(callback)
+    return this
+  }
+
+  onStart(callback: OnStartCallback) {
+    this.onStartCallbacks.push(callback)
+    return this
+  }
+
+  onEarlyUpdate(callback: OnUpdateCallback) {
+    this.onEarlyUpdateCallbacks.add(callback)
+    return this
+  }
+
+  onUpdate(callback: OnUpdateCallback) {
+    this.onUpdateCallbacks.add(callback)
+    return this
+  }
+
+  onLateUpdate(callback: OnUpdateCallback) {
+    this.onLateUpdateCallbacks.add(callback)
+    return this
+  }
+
+  onRender(callback: OnUpdateCallback) {
+    this.onRenderCallbacks.add(callback)
+    return this
+  }
+
+  onStop(callback: OnStopCallback) {
+    this.onStopCallbacks.add(callback)
+    return this
+  }
+
+  async start() {
+    console.log("✅ Starting App")
+
+    /* Execute and wait for initializers to complete */
+    await Promise.all(this.onLoadCallbacks.map((callback) => callback()))
+
+    /* Execute and wait for startupSystems to complete */
+    await Promise.all(this.onStartCallbacks.map((callback) => callback(this)))
+
+    return this
+  }
+
+  stop() {
+    console.log("⛔ Stopping App")
+    this.onStopCallbacks.emit(this)
+    return this
+  }
+}
