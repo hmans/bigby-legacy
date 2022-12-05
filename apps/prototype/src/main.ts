f8d8cf7e904e5208117effb0719b5efe2aafc69c
Hendrik Mans <hendrik@mans.de>
2022-12-05 13:48:36 +0100

    Cleanup

--- apps/prototype/src/main.ts
+++ apps/prototype/src/main.ts
@@ -20,33 +20,20 @@ const app = new App()
 await app.start()
 
 /* Camera */
-{
-  const camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-  )
+app.add([
+  new Transform3D([0, 0, 5]),
+  new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000),
+])
 
-  camera.position.z = 10
-
-  app.add([camera])
-}
 /* Lights */
-app.add([new Transform3D(), new THREE.AmbientLight(0xffffff, 0.2)])
-
-{
-  const dlight = new THREE.DirectionalLight(0xffffff, 1)
-  dlight.position.set(10, 20, 30)
-  app.add([dlight])
-}
+app.add([new THREE.AmbientLight(0xffffff, 0.2)])
+app.add([new Transform3D([10, 20, 30]), new THREE.DirectionalLight(0xffffff, 1)])
 
 /* Rotating cube */
-{
-  const mesh = new THREE.Mesh(
+app.add([
+  new AutoRotate(new Vector3(1, 2, 3)),
+  new THREE.Mesh(
     new THREE.BoxGeometry(1, 1, 1),
     new THREE.MeshStandardMaterial({ color: "hotpink" })
-  )
-
-  app.add([new AutoRotate(new Vector3(1, 2, 3)), mesh])
-}
+  ),
+])
