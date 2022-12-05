59e5f61de5a5aa07867749525c034001e824559e
Hendrik Mans <hendrik@mans.de>
2022-12-05 16:31:32 +0100

    Docs

--- packages/bigby-math/src/constants.ts
+++ packages/bigby-math/src/constants.ts
@@ -1,3 +1,13 @@
 export const EPSILON = 0.000001
 
+/**
+ * The magic game development number.
+ *
+ * > "Gamedev secret: All of your favorite games have a line
+ * > of code somewhere with an arbitrary magic number that's
+ * > just fed into some formula because it feels right. The
+ * > variable is called "fudge" and its value is always 0.65"
+ *
+ * (Aaron San Filippo)
+ */
 export const FUDGE = 0.65
