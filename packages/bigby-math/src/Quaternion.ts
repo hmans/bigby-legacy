dab3df6296fdda59f7c3cd51435659cdd131e4b4
Hendrik Mans <hendrik@mans.de>
2022-12-05 15:14:21 +0100

    Quaternion.setFromRotationMatrix

--- packages/bigby-math/src/Quaternion.ts
+++ packages/bigby-math/src/Quaternion.ts
@@ -1,3 +1,5 @@
+import { IMatrix4 } from "./Matrix4"
+
 export interface IQuaternion {
   x: number
   y: number
@@ -105,5 +107,51 @@ export class Quaternion implements IQuaternion {
     quat.w = w * bw - z * bz
 
     return quat
+  }
+
+  static setFromRotationMatrix(out: IQuaternion, m: IMatrix4) {
+    const te = m.elements,
+      m11 = te[0],
+      m12 = te[4],
+      m13 = te[8],
+      m21 = te[1],
+      m22 = te[5],
+      m23 = te[9],
+      m31 = te[2],
+      m32 = te[6],
+      m33 = te[10],
+      trace = m11 + m22 + m33
+
+    if (trace > 0) {
+      const s = 0.5 / Math.sqrt(trace + 1.0)
+
+      out.w = 0.25 / s
+      out.x = (m32 - m23) * s
+      out.y = (m13 - m31) * s
+      out.z = (m21 - m12) * s
+    } else if (m11 > m22 && m11 > m33) {
+      const s = 2.0 * Math.sqrt(1.0 + m11 - m22 - m33)
+
+      out.w = (m32 - m23) / s
+      out.x = 0.25 * s
+      out.y = (m12 + m21) / s
+      out.z = (m13 + m31) / s
+    } else if (m22 > m33) {
+      const s = 2.0 * Math.sqrt(1.0 + m22 - m11 - m33)
+
+      out.w = (m13 - m31) / s
+      out.x = (m12 + m21) / s
+      out.y = 0.25 * s
+      out.z = (m23 + m32) / s
+    } else {
+      const s = 2.0 * Math.sqrt(1.0 + m33 - m11 - m22)
+
+      out.w = (m21 - m12) / s
+      out.x = (m13 + m31) / s
+      out.y = (m23 + m32) / s
+      out.z = 0.25 * s
+    }
+
+    return this
   }
 }
