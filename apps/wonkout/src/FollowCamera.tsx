d64539bf36ac3b51770b858409b8132e6d9867fb
Hendrik Mans <hendrik@mans.de>
2022-12-05 14:45:25 +0100

    Some lookatting

--- apps/wonkout/src/FollowCamera.tsx
+++ apps/wonkout/src/FollowCamera.tsx
@@ -1,4 +1,4 @@
-import { App, Transform3D, Vector3 } from "bigby"
+import { App, Matrix4, Transform3D, Vector3 } from "bigby"
 import * as THREE from "three"
 
 export class CameraTarget {}
@@ -9,6 +9,8 @@ export function FollowCameraPlugin(app: App) {
   const cameraQuery = app.query([Transform3D, THREE.Camera])
   const targetQuery = app.query([Transform3D, CameraTarget])
 
+  const mat4 = new Matrix4()
+
   app.onLateUpdate(() => {
     const camera = cameraQuery.first
     const target = targetQuery.first
@@ -16,8 +18,16 @@ export function FollowCameraPlugin(app: App) {
     if (camera && target) {
       const targetTransform = target.get(Transform3D)!
       const cameraTransform = camera.get(Transform3D)!
-
       /* TODO: Rotate quaternion to look at target */
+
+      Matrix4.lookAt(
+        mat4,
+        cameraTransform.position,
+        targetTransform.position,
+        new Vector3(0, 1, 0)
+      )
+
+      Matrix4.copy(cameraTransform.matrix, mat4)
     }
   })
 
