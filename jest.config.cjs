0fc957073a50e0b49cfe88554ba1f0da097d15d8
Hendrik Mans <hendrik@mans.de>
2022-12-06 11:18:55 +0100

    Use new jest reporter

--- jest.config.cjs
+++ jest.config.cjs
@@ -4,5 +4,6 @@ module.exports = {
   testMatch: ["**/?(*.)+(spec|test).+(ts|tsx)"],
   testPathIgnorePatterns: ["node_modules"],
   testEnvironment: "jsdom",
-  moduleFileExtensions: ["js", "ts", "tsx"]
+  moduleFileExtensions: ["js", "ts", "tsx"],
+  reporters: ["jest-progress-bar-reporter"]
 }
