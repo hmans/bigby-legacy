380c953c765e4b1a69ce2868049d2f79dcd2709e
Hendrik Mans <hendrik@mans.de>
2022-12-06 16:06:09 +0100

    Walls

--- apps/wonkout/src/Walls.tsx
+++ apps/wonkout/src/Walls.tsx
@@ -5,13 +5,16 @@ import * as THREE from "three"
 export const Walls = (app: App) =>
   app.onStart((app) => {
     const height = 4
+
+    const material = new THREE.MeshStandardMaterial({ color: "#999" })
+
     /* North Wall */
     app.spawn([
       new Physics.StaticBody(),
       new Physics.BoxCollider([24, 1, height]).setDensity(0),
       make(THREE.Mesh, {
         geometry: new THREE.BoxGeometry(24, 1, height),
-        material: new THREE.MeshStandardMaterial({ color: "#999" }),
+        material,
         castShadow: true,
         receiveShadow: true,
         position: [0, 8.5, 0]
@@ -24,7 +27,7 @@ export const Walls = (app: App) =>
       new Physics.BoxCollider([1, 18, height]).setDensity(0),
       make(THREE.Mesh, {
         geometry: new THREE.BoxGeometry(1, 18, height),
-        material: new THREE.MeshStandardMaterial({ color: "#999" }),
+        material,
         castShadow: true,
         receiveShadow: true,
         position: [-12.5, 0, 0]
@@ -37,7 +40,7 @@ export const Walls = (app: App) =>
       new Physics.BoxCollider([1, 18, height]).setDensity(0),
       make(THREE.Mesh, {
         geometry: new THREE.BoxGeometry(1, 18, height),
-        material: new THREE.MeshStandardMaterial({ color: "#999" }),
+        material,
         castShadow: true,
         receiveShadow: true,
         position: [12.5, 0, 0]
@@ -51,7 +54,7 @@ export const Walls = (app: App) =>
       new Physics.BoxCollider([24, 1, height]).setDensity(0),
       make(THREE.Mesh, {
         geometry: new THREE.BoxGeometry(24, 1, height),
-        material: new THREE.MeshStandardMaterial({ color: "#999" }),
+        material,
         castShadow: true,
         receiveShadow: true,
         position: [0, -8.5, 0]
