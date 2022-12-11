8dc31fc7a3fc43a10244bdece71dcdf83a74b5bb
Hendrik Mans <hendrik@mans.de>
2022-12-11 12:48:21 +0100

    Refactor that class

--- packages/bigby-core/src/SystemsPlugin.ts
+++ packages/bigby-core/src/SystemsPlugin.ts
@@ -4,4 +4,14 @@ import { System } from "./System"
 export function SystemsPlugin(app: App) {
   app.registerComponent(System)
   const systems = app.query([System])
+
+  /* Invoke all systems' onStart methods */
+  app.onStart(() => {
+    for (const [_, system] of systems) system.onStart?.()
+  })
+
+  /* Invoke all systems' onStop methods */
+  app.onStop(() => {
+    for (const [_, system] of systems) system.onStop?.()
+  })
 }
--- packages/bigby-plugin-three/src/index.ts
+++ packages/bigby-plugin-three/src/index.ts
@@ -40,94 +40,84 @@ class ThreeSystem extends System {
     this.renderer.outputEncoding = THREE.sRGBEncoding
 
     this.scene = new THREE.Scene()
+  }
 
-    /* Renderer & Canvas */
-    app.onStart((app) => {
-      /* Create our renderer */
-      this.renderer.setSize(window.innerWidth, window.innerHeight)
-      document.body.appendChild(this.renderer.domElement)
+  onStart(): void {
+    /* Mount our renderer */
+    this.renderer.setSize(window.innerWidth, window.innerHeight)
+    document.body.appendChild(this.renderer.domElement)
+
+    /* Camera & Rendering */
+    const cameras = this.app.query([THREE.Camera])
+
+    /* When a new camera appears, register it as the main camera */
+    cameras.onEntityAdded.add((entity) => {
+      this.camera = entity.get(THREE.Camera)
     })
 
-    app.onStop(() => {
-      console.debug("Disposing renderer")
-      document.body.removeChild(this.renderer.domElement)
-      this.renderer.dispose()
-      this.renderer.forceContextLoss()
+    /* When a camera disappears and it's our active camera, disable it */
+    cameras.onEntityRemoved.add((entity) => {
+      if (entity.get(THREE.Camera) === this.camera) this.camera = undefined
+    })
+
+    /* Resize the renderer when the window resizes */
+    const onResize = () => {
+      this.renderer.setSize(window.innerWidth, window.innerHeight)
+
+      if (this.camera instanceof THREE.PerspectiveCamera) {
+        this.camera.aspect = window.innerWidth / window.innerHeight
+        this.camera.updateProjectionMatrix()
+      } else if (this.camera instanceof THREE.OrthographicCamera) {
+        this.camera.left = -window.innerWidth / 2
+        this.camera.right = window.innerWidth / 2
+        this.camera.top = window.innerHeight / 2
+        this.camera.bottom = -window.innerHeight / 2
+        this.camera.updateProjectionMatrix()
+      }
+    }
+
+    window.addEventListener("resize", onResize)
+
+    this.app.onStop(() => {
+      window.removeEventListener("resize", onResize)
     })
 
     /* Scene Objects */
-    app.onStart((app) => {
-      /* Query the world for Three.js scene objects */
-      const sceneObjects = app.query([THREE.Object3D])
+    /* Query the world for Three.js scene objects */
+    const sceneObjects = this.app.query([THREE.Object3D])
 
-      /* When an entity with a scene object appears, add it to the Three.js scene */
-      sceneObjects.onEntityAdded.add((entity) => {
-        const object3d = entity.get(THREE.Object3D)!
-        this.scene.add(object3d)
-      })
+    /* When an entity with a scene object appears, add it to the Three.js scene */
+    sceneObjects.onEntityAdded.add((entity) => {
+      const object3d = entity.get(THREE.Object3D)!
+      this.scene.add(object3d)
+    })
 
-      /* When an entity with a scene object disappears, remove it from the Three.js scene */
-      sceneObjects.onEntityRemoved.add((entity) => {
-        this.scene.remove(entity.get(THREE.Object3D)!)
-      })
+    /* When an entity with a scene object disappears, remove it from the Three.js scene */
+    sceneObjects.onEntityRemoved.add((entity) => {
+      this.scene.remove(entity.get(THREE.Object3D)!)
     })
 
     /* Custom Parenting */
-    app.onStart((app) => {
-      const parentedQuery = app.query([Parent3D, THREE.Object3D])
+    const parentedQuery = this.app.query([Parent3D, THREE.Object3D])
 
-      parentedQuery.onEntityAdded.add((entity) => {
-        const parent = entity.get(Parent3D)!.parent
-        const object = entity.get(THREE.Object3D)!
-        parent.add(object)
-      })
-
-      parentedQuery.onEntityRemoved.add((entity) => {
-        const parent = entity.get(Parent3D)!.parent
-        const object = entity.get(THREE.Object3D)!
-        parent.remove(object)
-      })
+    parentedQuery.onEntityAdded.add((entity) => {
+      const parent = entity.get(Parent3D)!.parent
+      const object = entity.get(THREE.Object3D)!
+      parent.add(object)
     })
 
-    /* Camera & Rendering */
-    app.onStart((app) => {
-      const cameras = app.query([THREE.Camera])
-
-      /* When a new camera appears, register it as the main camera */
-      cameras.onEntityAdded.add((entity) => {
-        this.camera = entity.get(THREE.Camera)
-      })
-
-      /* When a camera disappears and it's our active camera, disable it */
-      cameras.onEntityRemoved.add((entity) => {
-        if (entity.get(THREE.Camera) === this.camera) this.camera = undefined
-      })
-
-      /* Render every frame using the active camera if we have one */
-      app.spawn([new ThreeSystem(app)])
-
-      /* Resize the renderer when the window resizes */
-      const onResize = () => {
-        this.renderer.setSize(window.innerWidth, window.innerHeight)
-
-        if (this.camera instanceof THREE.PerspectiveCamera) {
-          this.camera.aspect = window.innerWidth / window.innerHeight
-          this.camera.updateProjectionMatrix()
-        } else if (this.camera instanceof THREE.OrthographicCamera) {
-          this.camera.left = -window.innerWidth / 2
-          this.camera.right = window.innerWidth / 2
-          this.camera.top = window.innerHeight / 2
-          this.camera.bottom = -window.innerHeight / 2
-          this.camera.updateProjectionMatrix()
-        }
-      }
-
-      window.addEventListener("resize", onResize)
-
-      app.onStop(() => {
-        window.removeEventListener("resize", onResize)
-      })
+    parentedQuery.onEntityRemoved.add((entity) => {
+      const parent = entity.get(Parent3D)!.parent
+      const object = entity.get(THREE.Object3D)!
+      parent.remove(object)
     })
+  }
+
+  onStop(): void {
+    console.debug("Disposing renderer")
+    document.body.removeChild(this.renderer.domElement)
+    this.renderer.dispose()
+    this.renderer.forceContextLoss()
   }
 
   onRender(): void {
