50a7a8ec428bf9a1b1ae13fa4ed685632f38c2d5
Hendrik Mans <hendrik@mans.de>
2022-12-11 13:23:10 +0100

    Cleanup

--- apps/wonkout/src/Player.tsx
+++ apps/wonkout/src/Player.tsx
@@ -8,6 +8,30 @@ export class PlayerComponent {
   angularThrust = 10
 }
 
+class PlayerSystem extends System {
+  protected playerQuery = this.app.query([PlayerComponent])
+
+  onUpdate(dt: number): void {
+    const player = this.playerQuery.first
+
+    if (player) {
+      const { move, aim } = player.get(Input)!
+      const rigidbody = player.get(Physics.RigidBody)!
+      const { thrust, angularThrust } = player.get(PlayerComponent)!
+
+      const rb = rigidbody.raw!
+      rb.resetForces(false)
+      rb.resetTorques(false)
+
+      /* Move */
+      rb.applyImpulse({ x: move.x * thrust, y: move.y * thrust, z: 0 }, true)
+
+      /* Rotate */
+      rb.applyTorqueImpulse({ x: 0, y: 0, z: aim.x * -angularThrust }, true)
+    }
+  }
+}
+
 export const Player = (app: App) => {
   app.registerComponent(PlayerComponent)
 
@@ -38,33 +62,6 @@ export const Player = (app: App) => {
         position: [0, -7, 0]
       })
     ])
-
-    const playerQuery = app.query([PlayerComponent])
-
-    class PlayerSystem extends System {
-      onUpdate(dt: number): void {
-        const player = playerQuery.first
-
-        if (player) {
-          const { move, aim } = player.get(Input)!
-          const rigidbody = player.get(Physics.RigidBody)!
-          const { thrust, angularThrust } = player.get(PlayerComponent)!
-
-          const rb = rigidbody.raw!
-          rb.resetForces(false)
-          rb.resetTorques(false)
-
-          /* Move */
-          rb.applyImpulse(
-            { x: move.x * thrust, y: move.y * thrust, z: 0 },
-            true
-          )
-
-          /* Rotate */
-          rb.applyTorqueImpulse({ x: 0, y: 0, z: aim.x * -angularThrust }, true)
-        }
-      }
-    }
 
     app.spawn([new PlayerSystem(app)])
   })
