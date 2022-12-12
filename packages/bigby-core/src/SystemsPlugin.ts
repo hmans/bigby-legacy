269d702cd1f7301da1072efcb3e4a66fe8b60bdc
Hendrik Mans <hendrik@mans.de>
2022-12-12 10:47:45 +0100

    Nice

--- packages/bigby-core/src/SystemsPlugin.ts
+++ packages/bigby-core/src/SystemsPlugin.ts
@@ -1,44 +1,41 @@
 import { App } from "./App"
 import { System } from "./System"
 
-export function SystemsPlugin(app: App) {
-  app.registerComponent(System)
-  const systems = app.query([System])
+export const SystemsPlugin = (app: App) =>
+  app
+    .registerComponent(System)
 
-  /* Invoke all systems' onStart methods */
-  // app.onStart(() => {
-  //   for (const [_, system] of systems) system.onStart?.()
-  // })
+    .onStart((app) => {
+      const systems = app.query([System])
 
-  /* Start systems once they show up */
-  /* TODO: if the app is already started! */
-  systems.onEntityAdded.add((entity) => {
-    const system = entity.get(System)!
-    system.onStart?.()
-  })
+      /* Invoke all systems' onStart methods */
+      systems.onEntityAdded.add((entity) => {
+        const system = entity.get(System)!
+        system.onStart?.()
+      })
 
-  /* Invoke all systems' onStop methods */
-  app.onStop(() => {
-    for (const [_, system] of systems) system.onStop?.()
-  })
+      /* Invoke all systems' onStop methods */
+      app.onStop(() => {
+        for (const [_, system] of systems) system.onStop?.()
+      })
 
-  app.onEarlyUpdateCallbacks.add((dt) => {
-    for (const [_, system] of systems) system.onEarlyUpdate?.(dt)
-  })
+      app.onEarlyUpdateCallbacks.add((dt) => {
+        for (const [_, system] of systems) system.onEarlyUpdate?.(dt)
+      })
 
-  app.onFixedUpdateCallbacks.add((dt) => {
-    for (const [_, system] of systems) system.onFixedUpdate?.(dt)
-  })
+      app.onFixedUpdateCallbacks.add((dt) => {
+        for (const [_, system] of systems) system.onFixedUpdate?.(dt)
+      })
 
-  app.onUpdateCallbacks.add((dt) => {
-    for (const [_, system] of systems) system.onUpdate?.(dt)
-  })
+      app.onUpdateCallbacks.add((dt) => {
+        for (const [_, system] of systems) system.onUpdate?.(dt)
+      })
 
-  app.onLateUpdateCallbacks.add((dt) => {
-    for (const [_, system] of systems) system.onLateUpdate?.(dt)
-  })
+      app.onLateUpdateCallbacks.add((dt) => {
+        for (const [_, system] of systems) system.onLateUpdate?.(dt)
+      })
 
-  app.onRenderCallbacks.add((dt) => {
-    for (const [_, system] of systems) system.onRender?.(dt)
-  })
-}
+      app.onRenderCallbacks.add((dt) => {
+        for (const [_, system] of systems) system.onRender?.(dt)
+      })
+    })
